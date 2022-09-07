8617b1b54627995fe5ed46f2e50ae0d6abc8f859
Remi Gau <remi_gau@hotmail.com>
2022-09-07 14:57:36 +0200

    use relative size to decide how pictures should look on screen

--- supporting_functions/postInitializationSetup.m
+++ supporting_functions/postInitializationSetup.m
@@ -9,7 +9,8 @@ function [cfg, myExpTrials] = postInitializationSetup(cfg, myExpTrials, myVidStr
     % (substract interFrameInterval/3 to make sure that flipping is done
     % at 3sec straight and not 1 frame later)
     cfg.timing.ISI = 3 - cfg.screen.ifi / 6;
-    cfg.timing.frameDuration = 1 / cfg.videoFrameRate - cfg.screen.ifi / 6;
+    cfg.timing.frameDuration = 1 / cfg.video.frameRate - cfg.screen.ifi / 6;
+
 
     talkToMe(cfg, '\nTurning images into textures.\n');
     stimNames = fieldnames(myVidStructArray);
@@ -27,5 +28,19 @@ function [cfg, myExpTrials] = postInitializationSetup(cfg, myExpTrials, myVidStr
     for t = 1:length(stimNames)
         myExpTrials(t).visualStimuli = myVidStructArray.(stimNames{t});
     end
+
+    %% set where to display videos depending on requested apparent size
+
+    % assuming all videos have the same size
+    cfg.video.height = size(myVidStructArray.(stimNames{1})(1).stimImage, 1);
+    cfg.video.width = size(myVidStructArray.(stimNames{1})(1).stimImage, 1);
+
+    cfg.video = degToPix('apparentHeight', cfg.video, cfg);
+    cfg.video.apparentWidthPix = cfg.video.width / cfg.video.height * cfg.video.apparentHeightPix;
+
+    cfg.screen.stimulusRect  = [cfg.screen.center(1) - cfg.video.apparentWidthPix / 2, ...
+                                cfg.screen.center(2) - cfg.video.apparentHeightPix / 2, ...
+                                cfg.screen.center(1) + cfg.video.apparentWidthPix / 2, ...
+                                cfg.screen.center(2) + cfg.video.apparentHeightPix / 2];    
 
 end
